f6067d87c68848988acdec4b053febc3f63d4ccc
Russell Purinton <russell.purinton@gmail.com>
2025-03-26 04:00:07 +0000

    2025-03-26 04:00:06

--- nasr_update.sh
+++
@@ -1,4 +0,0 @@
-rm -rf /usr/src/aifrus/fc2s/examples/export/*
-php /usr/src/aifrus/fc2s/examples/get_latest.php
-mv -f /usr/src/aifrus/fc2s/examples/export/* /usr/src/aifrus/nasr_sql_zips/archive/
-cd /usr/src/aifrus/nasr_sql_zips && /usr/bin/push
